37e5e4ab22da502f197ff062a7bbd27b2348ee8d
satyamXcode <jaiswalsatyam876@gmail.com>
2025-12-16 20:01:34 +0530

    Completed all our routes

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,8 +10,11 @@ const PORT = process.env.PORT || 5001;
 
 connectDB();
 
+// middleware
+app.use(express.json());
+
 app.use("/api/notes", notesRoutes);
 
-app.listen(5001, () => {
+app.listen(PORT, () => {
     console.log("Server started on PORT", PORT);
 })
