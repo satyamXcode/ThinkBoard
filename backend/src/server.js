e72bf0c863e4b91dfa920311031cd8be94fddc8c
satyamXcode <jaiswalsatyam876@gmail.com>
2025-12-16 18:25:44 +0530

    Setup database: Schema & Model

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,10 +6,12 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5001;
+
 connectDB();
 
 app.use("/api/notes", notesRoutes);
 
 app.listen(5001, () => {
-    console.log("Server started on PORT: 5001");
+    console.log("Server started on PORT", PORT);
 })
