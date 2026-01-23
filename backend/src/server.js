c3a637cbc98addf9daa28c248e385488f2061e82
satyamXcode <jaiswalsatyam876@gmail.com>
2026-01-23 18:58:27 +0530

    Added middleware: RateLimiting

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
+import rateLimiter from "./middleware/rateLimiter.js";
 
 dotenv.config();
 
@@ -11,7 +12,14 @@ const PORT = process.env.PORT || 5001;
 connectDB();
 
 // middleware
-app.use(express.json());
+app.use(express.json()); // this middleware will parse JSON bodies: req.body
+app.use(rateLimiter);
+
+// our simple custom middleware
+app.use((req, res, next) => {
+    console.log(`Req method is ${req.method} & Req URL is ${req.url}`);
+    next();
+})
 
 app.use("/api/notes", notesRoutes);
 
